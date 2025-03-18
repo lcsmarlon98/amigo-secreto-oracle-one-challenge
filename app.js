1ad186663a4b72ebfb308d49829e6d9c894d8ce7
marlon <lucas.marlon.3@globalhitss.com.br>
2025-03-18 18:00:01 -0300

    Implementação da validação do campo nome, verificando se está vazio

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 const lista_amigo = []
 
+const remover_mensagem_erro = () => {
+    const span_elemento = document.getElementById('text-error')
+    span_elemento.innerHTML = ""
+}
+
+const apresentar_messagem_erro_nome_vazio = () => {
+    const span_elemento = document.getElementById('text-error')
+    span_elemento.append('Por favor, insira um nome.')
+}
+
 const adicionarAmigo = () => {
+    remover_mensagem_erro()
+
     const input_elemento = document.getElementById('amigo')
     const nome = input_elemento.value
 
     if (nome) {
         lista_amigo.push(nome)
         console.log(lista_amigo)
+    } else {
+        apresentar_messagem_erro_nome_vazio()
     }
 }
