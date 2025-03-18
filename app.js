54b4446620ad36ad775549e971c7c82a5852342d
marlon <lucas.marlon.3@globalhitss.com.br>
2025-03-18 17:52:31 -0300

    Adicionando o valor do nome ao array lista_amigo

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 const lista_amigo = []
+
+const adicionarAmigo = () => {
+    const input_elemento = document.getElementById('amigo')
+    const nome = input_elemento.value
+
+    if (nome) {
+        lista_amigo.push(nome)
+        console.log(lista_amigo)
+    }
+}
